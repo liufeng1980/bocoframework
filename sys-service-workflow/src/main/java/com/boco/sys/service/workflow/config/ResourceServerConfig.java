fb3e3b517e14ea60fa7b83157c1aac2e385417f9
liufeng <liufeng.beijing@163.com>
2020-04-24 08:35:18 +0800

    微服务和activiti整合

--- sys-service-workflow/src/main/java/com/boco/sys/service/workflow/config/ResourceServerConfig.java
+++ sys-service-workflow/src/main/java/com/boco/sys/service/workflow/config/ResourceServerConfig.java
@@ -4,7 +4,6 @@ import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.core.io.ClassPathResource;
 import org.springframework.core.io.Resource;
-import org.springframework.security.config.annotation.authentication.configuration.EnableGlobalAuthentication;
 import org.springframework.security.config.annotation.method.configuration.EnableGlobalMethodSecurity;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.oauth2.config.annotation.web.configuration.EnableResourceServer;
@@ -12,6 +11,7 @@ import org.springframework.security.oauth2.config.annotation.web.configuration.R
 import org.springframework.security.oauth2.provider.token.TokenStore;
 import org.springframework.security.oauth2.provider.token.store.JwtAccessTokenConverter;
 import org.springframework.security.oauth2.provider.token.store.JwtTokenStore;
+
 
 import java.io.BufferedReader;
 import java.io.IOException;
--- sys-service-workflow/src/main/java/com/boco/sys/service/workflow/controller/WorkFlowController.java
+++ sys-service-workflow/src/main/java/com/boco/sys/service/workflow/controller/WorkFlowController.java
@@ -5,16 +5,21 @@ import com.boco.framework.web.BaseController;
 import com.boco.sys.service.api.workflow.WorkFlowControllerApi;
 
 
+import com.boco.sys.service.workflow.service.CallFlowService;
 import org.activiti.api.task.runtime.TaskRuntime;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.security.access.prepost.PreAuthorize;
+import org.springframework.transaction.annotation.Transactional;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 @RequestMapping("/flow")
 public class WorkFlowController extends BaseController implements WorkFlowControllerApi {
 
+    @Autowired
+    CallFlowService callFlowService;
     @Autowired
     TaskRuntime taskRuntime;
 
@@ -30,5 +35,70 @@ public class WorkFlowController extends BaseController implements WorkFlowContro
     @RequestMapping("/start")
     public String start(){
         return "aaa";
+    }
+
+
+    @RequestMapping(value = "/deploy", produces = "text/html; charset=utf-8")
+    public String deploy(){
+        //callFlowService.deploy();
+        return "流程部署";
+    }
+
+
+
+    /**
+     * 呼叫中心提交订单
+     * @return
+     */
+    @RequestMapping(value = "/submitBill", produces = "text/html; charset=utf-8")
+    public String submitBill(){
+        callFlowService.submitBill();
+        return "呼叫中心提交订单";
+    }
+
+
+
+    /**
+     * 2.运营科审核
+     * @return
+     */
+    @ResponseBody
+    @RequestMapping(value = "/YunYingKeAudit", produces = "text/html; charset=utf-8")
+    public String YunYingKeAudit(){
+        callFlowService.YunYingKeAudit();
+        return "运营科审核";
+    }
+
+    /**
+     * 3.高路公司审核
+     * @return
+     */
+    @ResponseBody
+    @RequestMapping(value = "/gaoLuAudit", produces = "text/html; charset=utf-8")
+    public String gaoLuAudit(){
+        callFlowService.gaoLuAudit();
+        return "高路公司审核";
+    }
+
+    /**
+     * 4.分公司审核
+     * @return
+     */
+    @ResponseBody
+    @RequestMapping(value = "/fenZhongXinAudit",produces = "text/html; charset=utf-8")
+    public String fenZhongXinAudit(){
+        callFlowService.fenZhongXinAudit();
+        return "高路公司审核";
+    }
+
+    /**
+     * 5.高路公司的反馈
+     * @return
+     */
+    @ResponseBody
+    @RequestMapping(value = "/gaoluFeedbackAudit", produces = "text/html; charset=utf-8")
+    public String gaoluFeedbackAudit(){
+        callFlowService.gaoluFeedbackAudit();
+        return "高路公司审核";
     }
 }
